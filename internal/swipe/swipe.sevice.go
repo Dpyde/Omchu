712b1f882a1b29181928c5e05178e7f398f74d95
gunkgink <methasith33@gmail.com>
2025-03-17 14:46:22 +0700

    fix name

--- internal/swipe/swipe.sevice.go
+++ internal/swipe/swipe.sevice.go
@@ -6,7 +6,7 @@ import (
 	"github.com/Dpyde/Omchu/internal/entity"
 )
 
-//primary port
+// primary port
 type SwipeService interface {
 	SwipeCheck(swipe *entity.Swipe, is_match *bool) error
 }
@@ -19,12 +19,12 @@ func NewSwipeService(repo SwipeRepository) SwipeService {
 	return &swipeServiceImpl{repo: repo}
 }
 
-func (s *swipeServiceImpl) SwipeCheck(swipe *entity.Swipe ,is_match *bool) error {
-	if(swipe.SwipedID == swipe.SwiperID) {
+func (s *swipeServiceImpl) SwipeCheck(swipe *entity.Swipe, is_match *bool) error {
+	if swipe.SwipedID == swipe.SwiperID {
 		return errors.New("You can't swipe yourself")
 	}
 	// Business logic...
-	if err := s.repo.Pud(swipe,is_match); err != nil {
+	if err := s.repo.Pud(swipe, is_match); err != nil {
 		return err
 	}
 	return nil
