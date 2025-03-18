0dbff24fe23a21ce045acd35cd75b228b1bef191
dostayesky <brightkav2@gmail.com>
2025-03-19 03:51:29 +0700

    add success false in err case

--- internal/swipe/swipe.handler.go
+++ internal/swipe/swipe.handler.go
@@ -19,18 +19,18 @@ func NewHttpSwipeHandler(service SwipeService) *HtttSwipeHandler {
 func (h *HtttSwipeHandler) SwipeCheck(c *fiber.Ctx) error {
 	var swipe entity.Swipe
 	if err := c.BodyParser(&swipe); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"success":false , "error": err.Error()})
 	}
 	var is_match bool
 	userIdStr,ok := c.Locals("UserId").(string)
-	if(!ok) {return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "No UserId in local"})}
+	if(!ok) {return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"success":false,"error": "No UserId in local"})}
 	userId, err := strconv.ParseUint(userIdStr, 10, 32)
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid UserId in local"})
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"success":false,"error": "Invalid UserId in local"})
 	}
 	swipe.SwiperID = uint(userId)
 	if err := h.service.SwipeCheck(&swipe, &is_match); err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"success":false,"error": err.Error()})
 	}
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
