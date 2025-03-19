1b0a23434551dfb3ed33f7a41b423112942fbb88
wutthichod <wutthichod@gmail.com>
2025-03-19 16:27:28 +0700

    improve response {success}

--- internal/chat/chat.handler.go
+++ internal/chat/chat.handler.go
@@ -19,7 +19,13 @@ func (h *HttpChatHandler) GetChat(c *fiber.Ctx) error {
 	chats, err := h.service.GetChat(userId)
 	if err != nil {
 		fmt.Println(err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"sucess": false,
+			"error":  err.Error(),
+		})
 	}
-	return c.Status(fiber.StatusOK).JSON(chats)
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true,
+		"data":    chats,
+	})
 }
